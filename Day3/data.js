0f6ad2576242fdb8f17cdb25f5120aa6e51a2279
ShivamMangal <shivammangal0708@gmail.com>
2025-09-02 10:13:39 +0530

    push daY3

---
+++ Day3/data.js
@@ -0,0 +1,21 @@
+const user = [
+  {
+    name: "shivam",
+    age: "23",
+    course: "mca",
+  },
+  {
+    name: "mangal",
+    age: "22",
+    course: "mca",
+  },
+  {
+    name: "skand",
+    age: "23",
+    course: "mca",
+  },
+];
+
+module.exports = {
+  user,
+};
